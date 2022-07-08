012e68ba071daa7472cf89fb235ddcc35dde52b6
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-08 12:58:30 -0300

    Requisito 4 concluido

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function balls() {
 }
 
 balls();
+
+function colorAleatorias() {
+  const allColors = document.getElementsByClassName('ball');
+
+  allColors[0].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+  allColors[1].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+  allColors[2].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+  allColors[3].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+  allColors[4].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+  allColors[5].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 255
+  )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+}
+
+colorAleatorias();
